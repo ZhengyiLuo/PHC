84b31a8958de0f59cc7724e139fd92b8fbe7618e
zhengyiluo <zhengyiluo5@gmail.com>
2023-12-14 16:36:15 -0500

    webdemo

--- scripts/demo/video_to_pose_server.py
+++ scripts/demo/video_to_pose_server.py
@@ -24,9 +24,57 @@ from datetime import datetime
 from torchvision import transforms as T
 import time
 from ultralytics import YOLO
+import scipy.interpolate as interpolate
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+  tf.config.experimental.set_memory_growth(gpu, True)
 
 det_model = YOLO("yolov8s.pt")
 # accepts all formats - image/dir/Path/URL/video/PIL/ndarray. 0 for webcam
+STANDING_POSE = np.array([[[-0.1443, -0.9426, -0.2548],
+         [-0.2070, -0.8571, -0.2571],
+         [-0.0800, -0.8503, -0.2675],
+         [-0.1555, -1.0663, -0.3057],
+         [-0.2639, -0.5003, -0.2846],
+         [-0.0345, -0.4931, -0.3108],
+         [-0.1587, -1.2094, -0.2755],
+         [-0.2534, -0.1022, -0.3361],
+         [-0.0699, -0.1012, -0.3517],
+         [-0.1548, -1.2679, -0.2675],
+         [-0.2959, -0.0627, -0.2105],
+         [-0.0213, -0.0424, -0.2277],
+         [-0.1408, -1.4894, -0.2892],
+         [-0.2271, -1.3865, -0.2622],
+         [-0.0715, -1.3832, -0.2977],
+         [-0.1428, -1.5753, -0.2303],
+         [-0.3643, -1.3792, -0.2646],
+         [ 0.0509, -1.3730, -0.3271],
+         [-0.3861, -1.1423, -0.3032],
+         [ 0.0634, -1.1300, -0.3714],
+         [-0.4086, -0.9130, -0.2000],
+         [ 0.1203, -0.8943, -0.3002],
+         [-0.4000, -0.8282, -0.1817],
+         [ 0.1207, -0.8087, -0.2787]]]).repeat(5, axis = 0)
+
+def fps_20_to_30(mdm_jts):
+    jts = []
+    N = mdm_jts.shape[0]
+    for i in range(24):
+        int_x = mdm_jts[:, i, 0]
+        int_y = mdm_jts[:, i, 1]
+        int_z = mdm_jts[:, i, 2]
+        x = np.arange(0, N)
+        f_x = interpolate.interp1d(x, int_x)
+        f_y = interpolate.interp1d(x, int_y)
+        f_z = interpolate.interp1d(x, int_z)
+        
+        new_x = f_x(np.linspace(0, N-1, int(N * 1.5)))
+        new_y = f_y(np.linspace(0, N-1, int(N * 1.5)))
+        new_z = f_z(np.linspace(0, N-1, int(N * 1.5)))
+        jts.append(np.stack([new_x, new_y, new_z], axis = 1))
+    jts = np.stack(jts, axis = 1)
+    return jts
 
 
 def xyxy2xywh(bbox):
@@ -46,9 +94,9 @@ def download_model(model_type):
     model_path = os.path.join(os.path.dirname(model_zippath), model_type)
     return model_path
 
-async def main():
-    global pose_mat, trans, dt, reset_offset, offset_height, superfast, j3d, j2d, num_ppl, bbox, frame
-    offset = 0
+def start_pose_estimate():
+    global pose_mat, trans, dt, reset_offset, offset_height, superfast, j3d, j2d, num_ppl, bbox, frame, fps
+    offset = np.zeros((5, 1))
 
     from scipy.spatial.transform import Rotation as sRot
     global_transform = sRot.from_quat([0.5, 0.5, 0.5, 0.5]).inv().as_matrix()
@@ -59,7 +107,7 @@ async def main():
     print('### Run Model...')
     
     # model = tf.saved_model.load(download_model('metrabs_mob3l_y4'))
-    model = hub.load('https://bit.ly/metrabs_s') # or _s
+    model = hub.load('https://bit.ly/metrabs_s') # or _l
 
     skeleton = 'smpl_24'
     joint_names = model.per_skeleton_joint_names[skeleton].numpy().astype(str)
@@ -74,19 +122,28 @@ async def main():
                 pred = model.estimate_poses(frame, tf.constant(bbox, dtype=tf.float32), skeleton=skeleton, default_fov_degrees=55, num_aug=1)
                 
                 dt = time.time() - t_s
-                print(f'\r {1/dt:.2f} fps', end='')
+                fps = 1/dt
+                
                 # camera = poseviz.Camera.from_fov(55, frame.shape[:2])
                 # viz.update(frame, pred['boxes'], pred['poses3d'], camera)
                 pred_j3d = pred['poses3d'].numpy()
-                num_ppl = pred_j3d.shape[0]
-                j3d[:num_ppl] = pred_j3d/1000
+                num_ppl = min(pred_j3d.shape[0], 5)
+                
+                j3d_curr = pred_j3d[:num_ppl]/1000
+                if num_ppl < 5:
+                    j3d[num_ppl:, 0, 0] = np.arange(5 - num_ppl) + 1
+                    
                 j2d =  pred['poses2d'].numpy()
                 t_s = time.time()
                 
                 if reset_offset:
-                    offset = - offset_height - j3d[:num_ppl, [0], 1]
+                    offset[:num_ppl] = - offset_height - j3d_curr[:num_ppl, [0], 1]
                     reset_offset = False
-                j3d[:num_ppl, :, 1] += offset
+                
+                j3d_curr[:offset.shape[0], ..., 1] += offset[:num_ppl]
+                
+                j3d = j3d.copy() # Trying to handle race condition
+                j3d[:num_ppl] = j3d_curr
             
 def get_max_iou_box(det_output, prev_bbox, thrd=0.9):
     max_score = 0
@@ -102,6 +159,7 @@ def get_max_iou_box(det_output, prev_bbox, thrd=0.9):
         if float(iou_score) > max_score:
             max_bbox = [float(x) for x in bbox]
             max_score = iou_score
+    
     if max_bbox is None:
         max_bbox = prev_bbox
 
@@ -147,6 +205,23 @@ def get_one_box(det_output, thrd=0.9):
 
     return max_bbox
 
+def commandline_input():
+    global pose_mat, trans, dt, reset_offset, offset_height, superfast, j3d, j2d, num_ppl, bbox, frame, fps
+    
+    while True:
+        command = input('Type a message to send to the server: ')
+        if command == 'exit':
+            print('Exiting!')
+            raise SystemExit(0)
+        elif command.startswith("r"):
+            splits = command.split(":")
+            if len(splits) > 1:
+                offset_height = float(splits[-1])
+            reset_offset = True
+        elif command.startswith("fps"):
+            print(fps)
+        else:
+            print("Unkonw command")
 
 def frames_from_webcam():
     global frame, images_acc, recording, j2d, bbox
@@ -156,6 +231,8 @@ def frames_from_webcam():
     while (cap.isOpened()):
         # Capture frame-by-frame
         ret, frame_orig = cap.read()
+        if not ret:
+            continue
         # x1, y1, x2, y2 = bbox
         detec_threshold = 0.6
         
@@ -166,9 +243,10 @@ def frames_from_webcam():
             yolo_out_xyxy = yolo_output[0].boxes.xyxy.cpu().numpy()
             
             bbox = np.stack([yolo_out_xyxy[:, 0], yolo_out_xyxy[:, 1], (yolo_out_xyxy[:, 2] - yolo_out_xyxy[:, 0]), (yolo_out_xyxy[:, 3] - yolo_out_xyxy[:, 1])], axis = 1)
-            x1, y1, x2, y2 = yolo_out_xyxy[0]
             
-            frame_orig = cv2.rectangle(frame_orig, (int(x1), int(y1)), (int(x2), int(y2)), (154, 201, 219), 5)
+            for i in range(len(yolo_out_xyxy)):
+                x1, y1, x2, y2 = yolo_out_xyxy[i]
+                frame_orig = cv2.rectangle(frame_orig, (int(x1), int(y1)), (int(x2), int(y2)), (154, 201, 219), 5)
             
         if not j2d is None:
             for pt in j2d.reshape(-1, 2):
@@ -299,13 +377,7 @@ async def talk_websocket_handler(request):
     return ws_talker
 
 
-def start_pose_estimate():
-    loop = asyncio.new_event_loop()  # <-- create new loop in this thread here
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(main())
-
-
-bbox, pose_mat, j3d, j2d, trans, dt, ws_talkers, reset_offset, offset_height, images_acc, recording, sim_talker, num_ppl = np.zeros([5, 4]), np.zeros([24, 3, 3]), np.zeros([5, 24, 3]), None, np.zeros([3]), 1 / 10, [], True, 0.92, deque(maxlen = 24000), False, None, 0
+bbox, pose_mat, j3d, j2d, trans, dt, ws_talkers, reset_offset, offset_height, images_acc, recording, sim_talker, num_ppl, fps= np.zeros([5, 4]), np.zeros([24, 3, 3]), np.zeros([5, 24, 3]), None, np.zeros([3]), 1 / 10, [], True, 0.92, deque(maxlen = 24000), False, None, 0, 0
 frame = None
 superfast = True
 # main()
@@ -313,8 +385,9 @@ app = web.Application(client_max_size=1024**2)
 app.router.add_route('GET', '/ws', websocket_handler)
 app.router.add_route('GET', '/ws_talk', talk_websocket_handler)
 app.router.add_route('GET', '/get_pose', pose_getter)
-# threading.Thread(target=frames_from_webcam, daemon=True).start()
+threading.Thread(target=frames_from_webcam, daemon=True).start()
 threading.Thread(target=start_pose_estimate, daemon=True).start()
+threading.Thread(target=commandline_input, daemon=True).start()
 print("=================================================================")
 print("r: reset offset (use r:0.91), s: start recording, e: end recording, w: write video")
 print("=================================================================")
