6b0a1f0f97152c4145e7e4c9aa930b21b99c57e8
Zhengyi <zhengyiluo5@gmail.com>
2024-08-17 20:09:31 -0400

    dataset dump complete

--- scripts/phc_act/create_phc_act_dataset.py
+++ scripts/phc_act/create_phc_act_dataset.py
@@ -6,6 +6,10 @@ import os.path as osp
 sys.path.append(os.getcwd())
 
 import joblib
+import numpy as np
+import h5py
+from tqdm import tqdm
+from collections import defaultdict
 
 if __name__ == "__main__":
     num_envs = 512
@@ -13,11 +17,13 @@ if __name__ == "__main__":
     action_noise_std = 0.05
     # dataset_path = "/hdd/zen/dev/meta/EgoQuest/data/amass/pkls/amass_isaac_im_train_take6_upright_slim.pkl"
     dataset_path = "/hdd/zen/dev/meta/EgoQuest/data/amass/pkls/amass_isaac_run_upright_slim.pkl"
+    motion_file_name = dataset_path.split("/")[-1].split(".")[0]
     exp_name = "phc_comp_3"
     dataset_full = joblib.load(dataset_path)
     num_envs = len(dataset_full) if len(dataset_full)  < 512 else 512
     num_runs = 3
 
+    # Creating dataset
     for i in range(num_runs):
         if i == 0:
             add_action_noise = False
@@ -30,7 +36,41 @@ if __name__ == "__main__":
                 env.collect_dataset=True  env.add_action_noise={add_action_noise}   env.action_noise_std={action_noise_std}"
         
         print(cmd)
-        os.system(cmd)
+        # os.system(cmd)
+
+    print("Done")
+    # Aggregrating the dataset into one file 
+    full_dataset = defaultdict(list)
+    pkl_files = glob.glob(f"output/HumanoidIm/{exp_name}/phc_act/{motion_file_name}/*.pkl")
+
+    no_concatenate_keys = []
+
+    for file in tqdm(pkl_files):
+        file_data = joblib.load(file)
+        for k, v in file_data.items():
+            full_dataset[k].append(v)
+
+    for key, value in full_dataset.items():
+        if key in  ["running_mean"]:
+            full_dataset[key] = value[0]
+        elif key == "config":
+            continue
+        else:
+            full_dataset[key] = np.concatenate(value, axis=0)
+
+    with h5py.File(f'output/HumanoidIm/{exp_name}/phc_act/phc_act_{motion_file_name}.h5', 'w') as hdf5_file:
+        metadata_dump = {}
+        for key, value in full_dataset.items():
+            # Write each array to the HDF5 file with gzip compression
+            print(key)
+            if key in ['obs', 'env_action', 'reset', "clean_action", "reset"]:
+                h5_dataset = hdf5_file.create_dataset(key, data=value, compression="gzip", compression_opts=9)
+            else:
+                metadata_dump[key] = value
+        joblib.dump(metadata_dump, f'output/HumanoidIm/{exp_name}/phc_act/phc_act_{motion_file_name}_metadata.pkl', compress=True)
+                
 
     import ipdb; ipdb.set_trace()
-    print("Done")
+    
+    joblib.dump(full_dataset, f'output/HumanoidIm/{exp_name}/phc_act/phc_act_{motion_file_name}.pkl', compress=True)
+
