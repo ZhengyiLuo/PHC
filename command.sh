9fa9e7602ebadf042ceb897f9087a9054a7b39a6
Zhengyi <zhengyiluo5@gmail.com>
2024-08-17 22:41:22 -0400

    collect dataset

--- command.sh
+++ command.sh
@@ -11,7 +11,7 @@ robot.real_weight_porpotion_boxes=False env.num_prim=3 \
 env.motion_file=./data/amass/pkls/amass_isaac_im_train_take6_upright_slim.pkl \
 env.models=['output/HumanoidIm/phc_3/Humanoid.pth'] env.num_envs=1024 \
 headless=True epoch=-1 test=True im_eval=True \
-env.collect_dataset=True  \
+collect_dataset=True  \
 env.add_action_noise=True \
 env.mlp_bypass=False \
 env.action_noise_std=0.05 \
