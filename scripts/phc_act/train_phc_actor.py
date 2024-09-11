c0410792eef702532a64f57c3cb1c7739e78ae11
kangnil <kangnil@andrew.cmu.edu>
2024-09-11 17:03:08 -0400

    phc_actor save and resume

--- scripts/phc_act/train_phc_actor.py
+++ scripts/phc_act/train_phc_actor.py
@@ -19,17 +19,26 @@ from datetime import datetime
 from phc.learning.mlp import MLP
 import h5py
 from tqdm import tqdm
+import glob
+import os
+from datetime import datetime
+from collections import defaultdict
+from tqdm import tqdm
+import joblib
+import numpy as np
+import random
+
 
 wandb.login()
 device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 print("Device:", device)
 
 class HumanoidDataset(Dataset):
-    def __init__(self, dataset_path, use_pretrained_running_mean = True):
-        meta_data = joblib.load(dataset_path + "_meta_data.pkl")
-        self.hdf5_files = h5py.File(dataset_path + ".h5", 'r') 
-        self.obs = torch.tensor(self.hdf5_files["obs"][:])
-        self.actions = torch.tensor(self.hdf5_files["clean_action"][:])
+    def __init__(self, dataset_path, metadata_path, use_pretrained_running_mean = True):
+        meta_data = joblib.load(metadata_path)
+        data = joblib.load(dataset_path)
+        self.obs = torch.tensor(data['obs'])
+        self.actions = torch.tensor(data['clean_action'])
 
         if use_pretrained_running_mean:
             self.running_mean = meta_data["running_mean"]["running_mean"].cpu().float()
@@ -46,14 +55,93 @@ class HumanoidDataset(Dataset):
     def __getitem__(self, idx):
         return self.obs[idx], self.actions[idx]
     
-    def __del__(self):
-        self.hdf5_file.close()
+def save_checkpoint(model, optimizer, epoch, loss, foldername, filename="checkpoint.pth"):
+    checkpoint_path = os.path.join(foldername, filename)
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'loss': loss,
+    }, checkpoint_path)
 
+def load_checkpoint(model, optimizer, foldername, ckpt="checkpoint.pth"):
+    checkpoint_path = os.path.join(foldername, ckpt)
+    if os.path.exists(checkpoint_path):
+        checkpoint = torch.load(checkpoint_path)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        for state in optimizer.state.values():
+            if isinstance(state, torch.Tensor):
+                state.data = state.data.to(device)
+            elif isinstance(state, dict):
+                for k, v in state.items():
+                    if isinstance(v, torch.Tensor):
+                        state[k] = v.to(device)
+        start_epoch = checkpoint['epoch'] + 1
+        loss = checkpoint['loss']
+        print(f"Resuming training from epoch {start_epoch}")
+        return model, optimizer, start_epoch, loss
+    else:
+        print("No checkpoint found. Starting training from scratch.")
+        return model, optimizer, 0, None 
 
-def train_model(model, device, criterion, optimizer, data_loader, num_epochs, foldername, save_frequency = 100):
+def sample_pkl(directory):
+    # Use glob to list all .pkl files in the directory
+    all_pkl_files = glob.glob(f"{directory}/*.pkl")
+
+    noise_0_005 = []
+    noise_0075_01 = []
+
+    for pkl_file in all_pkl_files:
+        split = pkl_file.split("/")[-1].split("_")
+        if split[1] == "False" or split[2] == "0.05":
+            noise_0_005.append(pkl_file)
+        else:
+            noise_0075_01.append(pkl_file)
+
+    pkl_files = random.sample(noise_0_005, 5) + random.sample(noise_0075_01, 1)
+    print("curr dataset: ", pkl_files)
+
+    full_dataset = defaultdict(list)
+
+    for file in tqdm(pkl_files):
+        file_data = joblib.load(file)
+        for k, v in file_data.items():
+            full_dataset[k].append(v)
+
+    for key, value in full_dataset.items():
+        if key in ["running_mean"]:
+            full_dataset[key] = value[0]
+        elif key == "config":
+            continue
+        else:
+            full_dataset[key] = np.concatenate(value, axis=0)
+
+    time = datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
+    save_file = f'{directory}/combined_pkl'
+    os.makedirs(save_file, exist_ok=True)
+    with open(f'{save_file}/{time}.pkl', 'wb') as f:
+        joblib.dump(full_dataset, f)
+
+    with open(f'{save_file}/{time}.txt', 'w') as f:
+        for file in pkl_files:
+            f.write(f"{file}\n")
+    del full_dataset
+    return f'{save_file}/{time}.pkl'
+
+def train_model(model, device, criterion, optimizer, batch_size,
+                dataset_path, metadata_path, sample_pkl_or_not,
+                start_epoch, num_epochs, foldername, save_frequency = 100):
     model.to(device)
-    pbar = tqdm(range(num_epochs))
+    pbar = tqdm(range(start_epoch, num_epochs))
+    if not sample_pkl_or_not:
+        dataset = HumanoidDataset(dataset_path, metadata_path, data=None)
+        data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=32)
     for epoch in pbar:
+        if epoch%save_frequency == 0 and sample_pkl_or_not:
+            dataset_path = sample_pkl(directory= "/data/kangnil/PHC/output/HumanoidIm/phc_comp_3/phc_act/amass_isaac_im_train_take6_upright_slim/")
+            dataset = HumanoidDataset(dataset_path, metadata_path)
+            data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=32)
         for batch_obs, batch_actions in data_loader:
             # Forward pass
             batch_obs, batch_actions = batch_obs.to(device), batch_actions.to(device)
@@ -72,20 +160,21 @@ def train_model(model, device, criterion, optimizer, data_loader, num_epochs, fo
         pbar.set_description(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
             
         if (epoch + 1) % save_frequency == 0:
-            torch.save(model.state_dict(), f'{foldername}/{epoch+1:05d}.pth')
-
+            #torch.save(model.state_dict(), f'{foldername}/{epoch+1:05d}.pth')
+            save_checkpoint(model, optimizer, epoch, loss, foldername, filename=f"{epoch+1:05d}.pth")
+            del data_loader
+            del dataset
     print("Training complete.")
     return model
 
-def train(dataset_path, output_path):
+
+
+def train(dataset_path, metadata_path, sample_pkl_or_not, output_path, ckpt_path="checkpoint.pth"):
     units = [2048, 1024, 512]  # Example hidden layer size
     batch_size = 16384
-    num_epochs = 1000
+    num_epochs = 100000
     save_frequency = 100
     learning_rate = 2e-5
-
-    dataset = HumanoidDataset(dataset_path)
-    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=32)
 
     run = wandb.init(
         # Set the project where this run will be logged
@@ -99,19 +188,23 @@ def train(dataset_path, output_path):
             "dataset": dataset_path,
         },
     )
-
-    # Instantiate the model, define the loss function and the optimizer
-    model = MLP(dataset.obs_size, dataset.action_size, units, "silu")
+    model = MLP(934, 69, units, "silu")
     criterion = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
+    model, optimizer, start_epoch, loss = load_checkpoint(model, optimizer, output_path, ckpt=ckpt_path)
+
     # Train the model
-    train_model(model, device, criterion, optimizer, data_loader, num_epochs, output_path, save_frequency = save_frequency)
+    train_model(model, device, criterion, optimizer, batch_size, dataset_path, metadata_path, sample_pkl_or_not, start_epoch, num_epochs, output_path, save_frequency = save_frequency)
 
 
 if __name__ == "__main__":
-    dataset_path = "output/HumanoidIm/phc_comp_3/phc_act/phc_act_amass_isaac_run_upright_slim"
-    output_path = "output/HumanoidIm/phc_comp_3/phc_act/models/"
+    dataset_path = "/data/kangnil/PHC/output/HumanoidIm/phc_comp_3/phc_act/amass_isaac_im_train_take6_upright_slim/0_and_0.05_noise/six_0.05/combined_pkl/2024-09-08-04:30:47.pkl"
+    #dataset_path = "/data/kangnil/PHC/output/HumanoidIm/phc_comp_3/phc_act/amass_isaac_im_train_take6_upright_slim/noise_True_0.1_2024-09-05-05:59:22.pkl"
+    metadata_path = "/data/kangnil/PHC/output/HumanoidIm/phc_comp_3/phc_act/phc_act_amass_isaac_im_train_take6_upright_slim_metadata.pkl"
+    output_path = f"/data/kangnil/PHC/output/HumanoidIm/phc_comp_3/phc_act/amass_isaac_im_train_take6_upright_slim/models/"
+    ckpt_path = "00600.pth"
     os.makedirs(output_path, exist_ok=True)
-    train(dataset_path, output_path)
+    sample_pkl_or_not = True
+    train(dataset_path, metadata_path, sample_pkl_or_not, output_path, ckpt_path)
 
